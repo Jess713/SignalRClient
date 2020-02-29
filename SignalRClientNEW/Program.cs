36aa03d1aafc3df8fe3d493ed18d8161b6a6992d
Jess <jesskim2613@gmail.com>
2020-02-29 02:31:34 -0800

    Add project files.

---
+++ SignalRClientNEW/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Client;
+
+
+namespace SignalRClientNEW
+{
+    class Program
+    {
+        //https://docs.microsoft.com/en-us/aspnet/signalr/overview/guide-to-the-api/hubs-api-guide-net-client
+
+        static void Main(string[] args)
+        {
+            using (var hubConnection = new HubConnection("http://localhost:8089/"))
+            {
+                IHubProxy myHubProxy = hubConnection.CreateHubProxy("MyHub");
+               
+                hubConnection.Start().ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        Console.WriteLine("There was an error opening the connection:{0}", task.Exception.GetBaseException());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Connected");
+                    }
+                }).Wait();
+
+                //myHubProxy.On("notify", data => Console.WriteLine("client: " + data + " was recieved from server"));
+               
+
+                myHubProxy.Invoke("Send", "HELLO World ").ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        Console.WriteLine("There was an error calling send: {0}", task.Exception.GetBaseException());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Send Complete.");
+                    }
+                });
+            }
+
+
+
+
+
+
+        }
+    }
+}
